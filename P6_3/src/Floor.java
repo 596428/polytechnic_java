e60ddad10685c06bcd15763c284d6f7bd25e7240
596428 <ttyr659@gmail.com>
2024-04-11 23:37:23 +0900

    game01.01
    //

--- P6_3/src/Floor.java
+++ P6_3/src/Floor.java
@@ -11,6 +11,7 @@ public class Floor {
 		this.floorName = name;
 	}
 	
+	// GameState에서 실행되는 메서드, enemy 객체들을 생성하고 engageInBattle 메서드를 실행
 	public void startBattle(List<Player> players) {
 		this.enemies = new ArrayList<>();
 		for(int i = 0; i< players.size(); i++) {
@@ -19,17 +20,22 @@ public class Floor {
 		engageInBattle(players, enemies);
 	}
 	
+	// 플레이어 혹은 몬스터가 전멸할때까지 번갈아가면서 공격을 주고받음
 	private void engageInBattle(List<Player> players, List<Enemy> enemies) {
 		int cnt = 0;
 		int damage = 0;
 		//플레이어 혹은 적의 리스트가 모두 사라질때까지 while문
 		while(!(players.isEmpty() ||enemies.isEmpty())) {
 			System.out.println((cnt+1) + "턴 시작");
+			// 플레이어 인덱스 순서대로, 목표 enemy를 잡고, attack 메서드로 데미지를 계산해서 underAttack으로 넘겨줌
+			// underattack에서는 데미지를 계산한 뒤 hp가 0이라면 객체 리스트에서 자기자신을 삭제함
+			// 최종적으로 어느 한쪽이 전멸하면 while문을 탈출함
 			for(Player player : players) {
 				Enemy targetEnemy = chooseTargetEnemy(player, enemies);
 				damage = player.attack();
 				targetEnemy.underAttack(damage);
 			}
+			// 모든 플레이어가 attack 메서드를 한번씩 실행하고 나면, enemy들이 한번씩 공격함
 			for(Enemy enemy : enemies) {
 				Player targetPlayer = chooseTargetPlayer(players);
 				damage = enemy.attack();
@@ -40,6 +46,7 @@ public class Floor {
 
 	}
 	
+	// 플레이어가 어떤 적을 선택할지는 이용자가 입력하도록 함
 	private Enemy chooseTargetEnemy(Player player, List<Enemy> enemies) {
         // enemies를 순회하면서 인덱스와 name를 출력하고, 번호를 입력받아서 리턴함
 		System.out.println("공격할 적의 번호를 입력하세요.");
@@ -63,6 +70,8 @@ public class Floor {
         return enemies.get(targetEnemyIndex);
     }
 	
+	// enemies는 다음과 같은 조건에 따라 목표 플레이어를 결정함
+	// 1. 현재 hp가 가장 낮은 플레이어 우선 2. hp가 동일하다면, 인덱스 번호가 높은 순서를 우선
 	private Player chooseTargetPlayer(List<Player> players) {
         Player targetPlayer = players.get(0);
         for (Player player : players) {
@@ -75,6 +84,7 @@ public class Floor {
         return targetPlayer;
     }
 	
+	// enemy는 floor에서 생성되기 때문에, enemy 객체의 삭제 역시 floor에서 처리함
 	public void removeEnemy(Enemy enemy) {
     	enemies.remove(enemy);
     }
--- P6_3/src/ThisIsNotAGame.java
+++ P6_3/src/ThisIsNotAGame.java
@@ -6,14 +6,14 @@ public class ThisIsNotAGame {
 		
 
     public void startGame() {
-        // GameState 생성하면 그 안에서 floor와 player 객체를 생성함
         this.gameState = new GameState();
+        // initializeGame 메서드로floor와 player 객체를 생성함
         this.gameState.initializeGame(maxFloor, playerCount);
         // climbingFloors 안쪽에서 게임 루프 실행
         this.gameState.climbingFloors();
 
 
-//      패배조건 : 모든 플레이어가 사망했다면
+        //패배조건 : 모든 플레이어가 사망했다면
         if (gameState.isGameOver()) {
         	System.out.println("Game Over!");
         } else {
